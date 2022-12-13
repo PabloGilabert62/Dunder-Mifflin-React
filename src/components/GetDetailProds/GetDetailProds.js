5498edde4a2e335b225ba7c2da1e48c1a220878f
Pablo Gilabert <gilabertpablo62@gmail.com>
2022-12-13 19:22:07 -0300

    123

--- src/components/GetDetailProds/GetDetailProds.js
+++ src/components/GetDetailProds/GetDetailProds.js
@@ -12,6 +12,7 @@ const GetDetailProds = () => {
 
     let {items, setItems} = useContext(CartContext)
     
+    const [count, setCount] = useState(0)
     const [prods, setProds] = useState({})
     const [isLoading, setIsLoading] = useState(true)
     const {alt} = useParams()
@@ -39,11 +40,12 @@ const GetDetailProds = () => {
     }
 
     const addItems = () => {
-        if(items < prods.stock){
-            setItems([...items, prods.id])
-        } else {
-            setItems(prods.stock)
+
+        if(items.length < prods.stock){
+            setItems([...items , prods.id])
         }
+
+        (count < prods.stock) ? setCount(count + 1): setCount(count)
     }
 
     /* -- GET EACH DETAIL CARD -- */
@@ -53,7 +55,7 @@ const GetDetailProds = () => {
                 <img src={prods.src} alt={prods.alt}/>
                 <h5 className='font-title'>{prods.title} {prods.price}</h5>
                 <p className='font-title'>Available stock: {prods.stock}</p>
-                <p className='font-title'>Items added: {items.length}</p>
+                <p className='font-title'>Items added: {count}</p>
                 <button onClick={addItems}>Add to cart</button>
             </div>
         </div>
