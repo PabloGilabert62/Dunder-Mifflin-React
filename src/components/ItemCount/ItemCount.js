8994f7dbcb87419c2dd700a4c22997cca8858ea9
Pablo Gilabert <gilabertpablo62@gmail.com>
2022-12-19 16:01:08 -0300

    123

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,2 +1,83 @@
 import React from "react";
+import { useState } from "react";
+import { getProdsByAlt } from '../../asyncMock';
+import { useParams } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
+import { FavoritesContext } from '../../context/FavoritesContext';
+import { useState, useEffect, useContext} from 'react';
 
+const ItemCount = () => {
+    const {isInFavorites} = useContext(FavoritesContext)
+    const {addItems} = useContext(CartContext)
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [showButton, setShowButton] = useState(true)
+    const {alt} = useParams()
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+
+        getProdsByAlt(alt)
+
+            .then(response => {
+                setProds(response)
+            })
+
+            .catch(error => {
+                console.error(error)
+            })
+
+            .finally(() => {
+                setIsLoading(false)
+            })
+
+    }, [alt])
+
+    if(isLoading) {
+        return <p className='loading'>Loading...</p>
+    }
+
+    /* ------------------------------------------------------------------------------- */
+    const isAdded = isInFavorites(prods.id)
+
+    /* ------------------------------------------------------------------------------- */
+    const add = () => { 
+        (count < prods.stock) ? setCount(count + 1): setCount(count)
+    }
+
+    /* ------------------------------------------------------------------------------- */
+    const substract = () => { 
+        (count > 0) ? setCount (count - 1) : setCount(count)
+    }
+
+    /* ------------------------------------------------------------------------------- */
+    const addToCart = () => {
+        if(count !== 0){
+            addItems({...prods, count})
+            setShowButton(false)
+        }
+    }
+
+    return(
+        {/* CART BUTTON */}
+
+        {showButton && 
+        <div>
+            <button onClick={() => {add()}} className='buttonPlus'>+</button>
+
+            <button onClick={() => {addToCart()}}>Add to cart</button>
+
+            <button onClick={() => {substract()}} className='buttonMinus'>-</button>
+        </div>}
+
+        {/* FAVORITE BUTTON */}
+        <button onClick={() => {isAdded ? removeFavorites(prods.id) : addFavorites(prods)}} 
+
+            className='buttonFavorite'>
+            {isAdded ? 'Remove from favorites' : 'Add to favorites'}
+
+        </button>   
+    )
+}
+
+export default ItemCount
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,91 +1,19 @@
 import './ItemDetail.css';
 import React from 'react';
-import { getProdsByAlt } from '../../asyncMock';
-import { useParams } from 'react-router-dom';
-import { useState, useEffect, useContext} from 'react';
-import { FavoritesContext } from '../../context/FavoritesContext';
-import { CartContext } from '../../context/CartContext';
+import { useState } from 'react';
 
 /* -- GET EACH DETAIL CARD -- */
 const ItemDetail = () => {
-    const {addFavorites, isInFavorites, removeFavorites} = useContext(FavoritesContext)
-    const {items, addItems} = useContext(CartContext)
-   
     const [prods, setProds] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
-    const [showButton, setShowButton] = useState(true)
-    const {alt} = useParams()
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-
-        getProdsByAlt(alt)
-
-            .then(response => {
-                setProds(response)
-            })
-
-            .catch(error => {
-                console.error(error)
-            })
-
-            .finally(() => {
-                setIsLoading(false)
-            })
-
-    }, [alt])
-
-    if(isLoading) {
-        return <p className='loading'>Loading...</p>
-    }
-
-    /* ------------------------------------------------------------------------------- */
-    const isAdded = isInFavorites(prods.id)
-
-    /* ------------------------------------------------------------------------------- */
-    const add = () => { 
-        (count < prods.stock) ? setCount(count + 1): setCount(count)
-    }
-
-    /* ------------------------------------------------------------------------------- */
-    const substract = () => { 
-        (count > 0) ? setCount (count - 1) : setCount(count)
-    }
-
-    /* ------------------------------------------------------------------------------- */
-    const addToCart = () => {
-        if(count !== 0){
-            addItems({...prods, count})
-            setShowButton(false)
-        }
-    }
-   
+    
     /* -- GET EACH DETAIL CARD -- */
     return (
         <div className='cards-details-flex'>
             <div className='cards-details'>
-
                 <img src={prods.src} alt={prods.alt}/>
                 <h5 className='font-title'>{prods.title} ${prods.price}</h5>
                 <p className='font-title'>Available stock: {prods.stock}</p>
-                <p className='font-title'>Items added:{count}</p>
-             
-                {/* CART BUTTON */}
-                {showButton && <div>
-                    <button onClick={() => {add()}} className='buttonPlus'>+</button>
-
-                    <button onClick={() => {addToCart()}}>Add to cart</button>
-
-                    <button onClick={() => {substract()}} className='buttonMinus'>-</button>
-                </div>}
-
-                {/* FAVORITE BUTTON */}
-                <button onClick={() => {isAdded ? removeFavorites(prods.id) : addFavorites(prods)}} 
-
-                    className='buttonFavorite'>
-                    {isAdded ? 'Remove from favorites' : 'Add to favorites'}
-
-                </button>   
+                <p className='font-title'>Items added:</p>
             </div>
         </div> 
     )
