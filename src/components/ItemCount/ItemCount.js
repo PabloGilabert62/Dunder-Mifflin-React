4c660dd0b28152d802319ee111241df0fabfa79f
Pablo Gilabert <gilabertpablo62@gmail.com>
2022-12-19 20:56:29 -0300

    123

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -3,10 +3,15 @@ import React, {useState } from "react";
 import { getProdsByAlt } from '../../asyncMock';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useContext } from 'react';
+import { FavoritesContext } from '../../context/FavoritesContext';
 
 export default function ItemCount ({stock, initial, text, onAddToCart}) {
 
+    const {addFavorites, removeFavorites, isInFavorites} = useContext(FavoritesContext)
+
     const [isLoading, setIsLoading] = useState(true)
+    const [showButton, setShowButton] = useState(false)
     const [prods, setProds] = useState({})
     const [count, setCount] = useState(initial);
     const {alt} = useParams()
@@ -29,6 +34,12 @@ export default function ItemCount ({stock, initial, text, onAddToCart}) {
 
     }, [alt])
 
+    const isAdded = () => {
+        if (!isInFavorites(prods.id)) {
+            setShowButton(true)
+        }
+    }
+
     function handleSubstract() {
         if (count > 1) setCount(count - 1);
     }
@@ -44,6 +55,11 @@ export default function ItemCount ({stock, initial, text, onAddToCart}) {
             <button onClick={handleAdd} className='buttonPlus'>+</button>
             <button onClick={addToCart}>Add to cart</button>
             <button onClick={handleSubstract} className='buttonMinus'>-</button>
+
+            <button onClick={() => {showButton ? removeFavorites(prods.id) : addFavorites(prods)}}
+                    className='buttonFavorite'>
+                    {showButton ? 'Remove from favorites' : 'Add to favorites'}
+            </button>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,37 +2,20 @@ import './ItemDetail.css';
 import React from 'react';
 import { useState } from "react";
 import { CartContext } from '../../context/CartContext';
-import { FavoritesContext } from '../../context/FavoritesContext';
 import { useContext } from 'react';
 import ItemCount from '../ItemCount/ItemCount';
 
 export default function ItemDetail ({prods, count}) {
 
-    const {addFavorites, removeFavorites, isInFavorites} = useContext(FavoritesContext)
-    const {isInCart} = useContext(CartContext)
-
-    const [showButton, setShowButton] = useState(false)
-    
-    const isAdded = () => {
-        if (!isInFavorites(prods.id)) {
-            setShowButton(true)
-        }
-    }
-
     return(
         <div className='cards-details-flex'>
             <div className='cards-details '>
-                
                 <img src={prods.src} alt={prods.alt}/>
                 <p className='font-title'>{prods.title} ${prods.price}</p>
                 <p className='font-title'>Available stock: {prods.stock}</p>
                 <p className='font-title'>Items added:{}</p>
-
-                <button onClick={() => {showButton ? removeFavorites(prods.id) : addFavorites(prods)}}
-                    className='buttonFavorite'>
-                    {showButton ? 'Remove from favorites' : 'Add to favorites'}
-                </button>
-            </div> 
+                <ItemCount/>
+            </div>
         </div>
     )
 }
